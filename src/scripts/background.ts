714701114d75f881dae2eae2a09187c1aa0ce740
hykwtakumin <hykwtakumin@gmail.com>
2019-04-11 09:18:32 +0900

    SVGの差し替えができた

---
+++ src/scripts/background.ts
@@ -0,0 +1,20 @@
+import chromep from "chrome-promise";
+import Tab = chrome.tabs.Tab;
+
+console.log("this is background");
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+    const msg = message;
+    if (msg.tag === "getImg") {
+        const request = await fetch(msg.body.url as string, {method: 'GET', mode: 'cors'});
+        const svg = await request.text();
+        console.dir(await svg);
+        const response = {
+            tag: "insertSVG",
+            body: svg
+        };
+        const activeTab = await chromep.tabs.query({active: true}) as Tab[];
+        const targetId = activeTab[0].id;
+        chrome.tabs.sendMessage(targetId, response);
+    }
+});
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,3 +1,48 @@
-window.onload = () => {
 
+window.onload = () => {
+    const observer = new MutationObserver(async (records: MutationRecord[]) => {
+        const addedEvent = records.find((record: MutationRecord) => {
+            const targetElm = record.target as HTMLElement;
+            return targetElm.className === "modal-open"
+        });
+
+        if (addedEvent) {
+            const modalElm = document.getElementsByClassName("modal-body");
+            if (modalElm) {
+                const modalBody = modalElm[0] as HTMLElement;
+                const link = modalBody.firstChild as HTMLLinkElement;
+                const img = link.firstChild as HTMLImageElement;
+
+
+                console.dir(link.href);
+
+                /*SVG以外の場合はここがnullでdrawButtonがつく*/
+                if (link.href) {
+                    /*子要素のimgを削除する*/
+                    link.removeChild(link.firstChild);
+                    /*代わりにSVGを差し込む*/
+                    const request = {
+                        tag: "getImg",
+                        body: {
+                            url: link.href
+                        }
+                    };
+                    chrome.runtime.sendMessage(request);
+
+                    chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+                        console.dir(message);
+                        if (message.tag === "insertSVG") {
+                            link.insertAdjacentHTML("afterbegin", message.body);
+                        }
+                    });
+                }
+
+
+                console.dir(img);
+            }
+
+        }
+    });
+    const observeOption = {childList: true};
+    observer.observe(document.body, observeOption);
 };
